5c8d867ba17188bdf887a247ebbf3407598b90f8
Kathirr007 <kathirr007@gmail.com>
2016-09-22 09:13:36 +0530

    Updated dynamic_cnt_body template file to make Dynamic slider work. Now Slider is wroking fine.

--- source/master/template/js/_dynamic_cnt_body.js
+++ source/master/template/js/_dynamic_cnt_body.js
@@ -32,14 +32,39 @@ var gall = arr[i].galleries,
 console.log(arr);
 $.each(arr,function(idx){
 if(arr[idx].placeid == pathPid) {
+var sliderImagesList = arr[idx].galleries;
+console.log(sliderImagesList);
+function buildSlider(sliderImagesList){
+	var sliderContent = '';
+	sliderContent += '<div id="jssor_1" style="position: relative; margin: 0 auto; top: 0px; left: 0px; width: 1300px; height: 500px; overflow: hidden; visibility: hidden;">\
+		  <div data-u="slides" style="cursor: default; position: relative; top: 0px; left: 0px; width: 1300px; height: 500px; overflow: hidden;">';
+	sliderImagesList.forEach(function(item){
+		var img_name = encodeURI(item.img_name);
+console.log(img_name);
+		sliderContent += '<div data-p="225.00" style="display: none;"><img data-u="image" src="'+item.img_name+'"/></div>';
+	});
+		sliderContent += '<div data-p="225.00" data-po="80% 55%" style="display: none;"> <a href="http://theoreminc.net"><img data-u="image" src="master/images/theorem.jpg"/></a></div> \
+		</div>\
+		  <div data-u="navigator" class="jssorb05" style="bottom:16px;right:16px;" data-autocenter="1">\
+			<div data-u="prototype" style="width:16px;height:16px;"></div>\
+		  </div>\
+		  <span data-u="arrowleft" class="jssora22l" style="top:0px;left:12px;width:40px;height:58px;" data-autocenter="2"></span> \
+		  <span data-u="arrowright" class="jssora22r" style="top:0px;right:12px;width:40px;height:58px;" data-autocenter="2"></span> \
+		</div>';
+console.log(sliderContent);		
+	$("#slider-content").append(sliderContent);
+}	
+	buildSlider(sliderImagesList);
+	jssor_1_slider_init();
+	
 out2 += "<p style=\"color:#b65c39; font-family: 'Abel', sans-serif; font-size:16px; font-weight:bold; padding:10px;\">" + arr[idx].title + "</p></div>";
 out2 += "</div>";
 out3 += "<div>"+ $('<div/>').html(arr[idx].details).text() +"</div>";
 out3 += "</div>";
 maps += "</div>";
  
-var lati = arr[idx].lati.substr(0, 8);
-var longi = arr[idx].longi.substr(0, 8);
+var lati = arr[idx].lati.substr(0, 7);
+var longi = arr[idx].longi.substr(0, 7);
 console.log ("selected page Latitude is " + lati + " & Longitude is " + longi);
 
 var center = new google.maps.LatLng(lati, longi);
