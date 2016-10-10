ab4c638e949eb3eafa2bed81d35ab1a6a31c1429
Kathirr007 <kathirr007@gmail.com>
2016-10-11 01:30:23 +0530

    Listing pages and serach results script updated + search list styles updated to fit in all devices

--- source/assets/template/js/_listing_search.js
+++ source/assets/template/js/_listing_search.js
@@ -30,21 +30,12 @@ $(document).on("keyup", "input[name='q']", function() {
             }
 
             if (searchTitle.indexOf(searchQuery) != -1) {
-                searchResult += '<a href=' + pathname + 'detail.html?pid=' + arr[i].placeid + ' style="text-decoration:none;">\
-								<div id="placeblocks">\
-									<div style="background-color:rgba(250, 250, 250, 1.0);">\
-										<div class="text">\
-											<ul>\
-											<li style="width:40%;">\
-												<img src="' + thumbImage + ' " alt="" width="125" height="75" class="img" style="border:2px solid #d1cece;">\
-											</li>\
-											<li style="width:60%; line-height:18px;">\
-												<span style="color:#448802; font-family:Abel, sans-serif; font-size:16px; font-weight:bold;"> "' + arr[i].title + '"</span><br>\
-												<span style="color:#222222; font-family:Abel, sans-serif; font-size:14px;"> "' + arr[i].address + '"</span>\
-												<div style="color:#000; padding-top:15px;"><ul style="float:left;"><li>' + getstars(ratingstars) + '</li><li>' + kms + 'Kms from city</li></ul></div>\
-											</li>';
-
-                searchResult += '</ul></div></div></div><div class="clear"></div><div class="clear"></div></a>';
+                searchResult += '<li class="list-items"><a class="clearfix" style="display:block;" href="' + pathname + 'detail.html?pid='+arr[i].placeid+'">\
+            <section class="list-contents clearfix"><figure><img src="'+thumbImage+'" alt="" width="125" height="75" /></figure>\
+            <aside><h4>'+arr[i].title+'</h4><p>'+arr[i].address+'</p><figure class="ratingstars">'+getstars(ratingstars)+'</figure>'+kms+'Kms from city</aside>\
+            </section>\
+            </a>\
+            </li>';
             }
         });
         if (searchResult == '') {
